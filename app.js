7a4747d52d64f3f10bd9b4bb23a06fcb72b9f5af
spm <spmcginnis@users.noreply.github.com>
2021-03-25 15:59:24 -0500

    added four starting hands example to app;

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ let Stack = require("./stack").Stack
 let Hand = require("./hand").Hand
 let Card = require("./card").Card
 
-let stack = new Stack()
+let stack = new Stack().shuffle()
 
-for (let card of stack.deck) {
-    console.log(card.toString())
+// for (let card of stack.deck) {
+//     console.log(card.toString())
+// }
+
+// TEMP Dealer and Player Hands 
+dealerHand = new Hand()
+allHands = []
+players = 3
+for (let i = 0; i<players; i++) {
+    let hand = new Hand()
+    allHands.push(hand)
+}
+allHands.push(dealerHand)
+
+for (let hand of allHands){
+    let card = stack.drawOne()
+    hand.addCard(card)
+}
+for (let hand of allHands){
+    let card = stack.drawOne()
+    hand.addCard(card)
 }
 
+for (let i=0; i<allHands.length; i++){
+    let handHolder = (i < allHands.length - 1) ? `player ${i+1}` : "dealer"
+    console.log(`${handHolder} total is ${allHands[i].getRunningTotal()}`)
+}
 
 // For testing // TODO move to testing suite
 function handTotal (listOfValues) {
