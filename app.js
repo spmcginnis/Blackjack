da80e470bbdcaef408fcad8b66226fb3ccd5d19d
spm <spmcginnis@users.noreply.github.com>
2021-05-24 21:16:22 -0500

    added async functionality to the app.js main game loop

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { Stack } = require("./stack")
 const { Player } = require("./player")
 const Game = require("./game")
+const readline = require('readline');
+// https://nodejs.org/api/readline.html#readline_event_line
+
+
 
 let stack = new Stack().shuffle()
 // TODO implement multiple decks in a 'shoe'
@@ -12,8 +16,49 @@ let playerList = [
     new Player("Terry", 100)]
 
 // Move set the table here?
+let round = 1
+let maxRounds = 10
 
-Game.round(playerList, stack)
+function prompt() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    return new Promise((resolve, reject) => {
+        rl.question('Play a round? Y/N ', (input) => {
+            rl.close()
+            resolve(input)
+        })
+    })
+}
+
+async function mainLoop () {
+    for (let i=0; i<maxRounds; i++)
+    {   
+        console.log("Loop has run ")
+        await prompt().then((input) => {
+            if (input === "Y")
+            {
+                console.log('Round ' + round++)
+                Game.round(playerList, stack)
+                //check stack
+                if (stack.deck.length < 20)
+                {
+                    stack = new Stack().shuffle()
+                    console.log("we have shuffled a new deck")
+                }
+            } else {
+                console.log("Process exited")
+                process.exit(0)
+            }
+        })
+    
+    
+    }
+}
+
+mainLoop()
+
 
 // Blackjack detections
 // Dealer
